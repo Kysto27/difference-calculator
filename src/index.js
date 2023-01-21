656a1ae81714122dda6b5907b5c14126f91dc926
Aleksandr R <kysto27@gmail.com>
2023-01-21 19:39:36 +0300

    step 5 add YAML

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 /* eslint-disable no-restricted-syntax */
+import fs from 'fs';
+import path from 'path';
 import _ from 'lodash';
 import parse from './parsers.js';
 
 const diff = (filepath1, filepath2) => {
-  const obj1 = parse(filepath1);
-  const obj2 = parse(filepath2);
+  const extension1 = path.extname(filepath1);
+  const extension2 = path.extname(filepath2);
+  const data1 = fs.readFileSync(path.resolve(process.cwd(), filepath1).trim());
+  const data2 = fs.readFileSync(path.resolve(process.cwd(), filepath2).trim());
+  const obj1 = parse(data1, extension1);
+  const obj2 = parse(data2, extension2);
   const keys1 = Object.keys(obj1);
   const keys2 = Object.keys(obj2);
   const keys = _.union(keys1, keys2);
--- src/parsers.js
+++ src/parsers.js
@@ -1,20 +1,17 @@
-import fs from 'fs';
-import path from 'path';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { load } from 'js-yaml';
 
-const parse = (filepath) => {
-  let result;
-  const extension = path.extname(filepath);
-  const readFile = fs.readFileSync(path.resolve(process.cwd(), filepath).trim());
-  if (extension === '.json') {
-    result = JSON.parse(readFile);
-  } else if (extension === '.yml') {
-    result = load(readFile);
-  } else if (extension === '.yaml') {
-    result = load(readFile);
+const parse = (data, extension) => {
+  switch (extension) {
+    case '.json':
+      return JSON.parse(data);
+    case '.yml':
+      return load(data);
+    case '.yaml':
+      return load(data);
+    default:
+      throw new Error(`Unknown extension: '${extension}'!`);
   }
-  return result;
 };
 
 export default parse;
