887d88978128f57888c8ad22aa49cab6a2441c29
Aleksandr R <kysto27@gmail.com>
2023-02-07 16:25:08 +0300

    edit index.js switch case default

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,15 +5,12 @@ import genDiff from '../src/index.js';
 const program = new Command();
 
 program
-  .version('0.0.1')
-  .description('Compares two configuration files and shows a difference.')
-  .argument('<filepath1>')
-  .argument('<filepath2>')
-  .option('-f, --format <type>', 'output format', 'stylish')
+  .version('0.0.1', '-v, --version', 'output the version number')
+  .description('Compares two configuration files and show a diference.')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format', 'stylish', 'stylish')
   .action((filepath1, filepath2) => {
     console.log(genDiff(filepath1, filepath2, program.opts().format));
   });
-
-// .parse(process.argv);
 
 program.parse();
